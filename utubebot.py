87236159be8cb1904735986ee6182ab318e22683
mintu1234567 <107040375+mintu1234567@users.noreply.github.com>
2022-06-12 02:32:12 +0530

    Add files via upload

---
+++ utubebot.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+
+from .config import Config
+
+
+class UtubeBot(Client):
+    def __init__(self):
+        super().__init__(
+            'my_bot',
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            plugins=dict(root="bot.plugins"),
+            workers=6,
+        )
+        self.DOWNLOAD_WORKERS = 6
+        self.counter = 0
+        self.download_controller = {}
